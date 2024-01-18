7af444bbc5058b520d19f034078006310fd762b0
Jerome Olvera <jerome.olvera@gmail.com>
2024-01-18 17:59:19 -0600

    add CleanCode

---
+++ Cf.Dotnet.PracticesAndPatterns.CleanCode/Program.cs
@@ -0,0 +1,15 @@
+﻿int areaTotal;
+int AreaTotal; // El compilador no se queja, pero no es la convencion.
+var aT = 0; // No es descriptivo. Que significa a?, Que significa T?
+
+// Se puede inferir el tipo de la variable
+var orderComplete = false;
+
+// NO se puede inferir el tipo de la variable
+var alumnos = GetClients();
+
+// Metodo unicamente para fines del ejemplo
+static List<int> GetClients()
+{
+    return [1, 2, 3];
+} 
